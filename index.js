469f46980c98f3a230146647b58536cdb677e5f4
Justin Brubaker <justinbrubaker7@gmail.com>
2021-04-12 17:58:25 -0700

    added promts and functions for intern and engineer

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const Manager = require('./lib/manager.js');
 const Engineer = require('./lib/engineer.js');
 const Intern = require('./lib/intern.js');
 
-
+//starts application
 function startApp(){
   getManager();
 }
 
+//runs the promtps to get manager information
 function getManager(){
   inquirer.prompt([
   {
@@ -38,12 +39,92 @@ function getManager(){
 })
 }
 
-nextEmployee(){
-  
+
+//this funtion gives the options to select another employee or generate the page
+function nextEmployee(){
+  inquirer.prompt({
+    type: "list",
+    name: "nextEmployee",
+    choices: [
+      "Intern",
+      "Engineer",
+      "No more employyes to add to the team"
+    ]
+  }).then((answers) => {
+    console.log(answers)
+    
+  if(answers.nextEmployee === "Intern"){
+    getIntern();
+  } else if(answers.nextEmployee === "Engineer"){
+    getEngineer();
+  } else {
+    generateHtml();
+  }
+  })
 }
 
 
+function getIntern(){
+  inquirer.prompt([
+    {
+      type: "Input",
+      name: "internName",
+      message: "What is the intern's name?"
+    },
+    {
+      type: "Input",
+      name: "interEmail",
+      message: "What is the intern's email?"
+    },
+    {
+      type: "Input",
+      name: "internId",
+      message: "What is the intern's ID number?"
+    },
+    {
+      type: "Input",
+      name: "internSchool",
+      message: "What is the intern school?"
+    },
+  ]).then((answers) => {
+    let intern = new Intern(answers.internName, answers.interEmail, answers.internId,answers.internSchool)
+    console.log(intern)
+    nextEmployee()
+  })
+};
 
 
+function getEngineer(){
+  inquirer.prompt([
+    {
+      type: "Input",
+      name: "engineerName",
+      message: "What is the engineer's name?"
+    },
+    {
+      type: "Input",
+      name: "engineerEmail",
+      message: "What is the engineer's email?"
+    },
+    {
+      type: "Input",
+      name: "engineerId",
+      message: "What is the engineer's ID number?"
+    },
+    {
+      type: "Input",
+      name: "engineerGithub",
+      message: "What is the engineers github username?"
+    },
+  ]).then((answers) => {
+    let engineer = new Engineer(answers.engineerName, answers.engineerEmail, answers.interengineerIdnId,answers.engineerGithub)
+    console.log(engineer)
+    nextEmployee()
+  })
+};
+
+// generateHtml(){
+
+// };
 
 startApp()
